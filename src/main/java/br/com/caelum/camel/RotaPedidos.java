5103d8173c1276984ff856e742cbe8d1690a68e9
xubuntu <xubuntu@xubuntu>
2018-04-22 15:34:59 -0300

    Aula 3

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,6 +1,7 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -13,17 +14,19 @@ public class RotaPedidos {
 			
 			@Override
 			public void configure() throws Exception {
-				from("file:pedidos?noop=true&delay=5s")
+				from("file:pedidos?noop=true&delay=5s").
+				setProperty("pedidoId",xpath("/pedido/id/text()")).
+				setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()"))
 				.split()
 					.xpath("/pedido/itens/item")
 				.filter()
 					.xpath("/item/formato[text()='EBOOK']")
 				.marshal()
 				.xmljson()
-				.log("${exchange.pattern}")
-				.log("${id} - ${body}")
-				.setHeader("CamelFileName",simple("${file:name.noext}.json"))
-				.to("file:saida");
+//				.log("${exchange.pattern}")
+				.log("${id} - ${body}").
+				setHeader(Exchange.HTTP_QUERY, simple("clienteId=${property.clienteId}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}"))				
+				.to("http4://localhost:8080/webservices/ebook/item");
 			}
 		});
 		context.start();
